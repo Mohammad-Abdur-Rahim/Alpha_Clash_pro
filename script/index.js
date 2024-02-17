b222a46ba86140b5d7b425ebc7183ab8013b4e10
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-17 11:29:11 +0600

    **js link section add/remove With Functio ***

--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 // JavaScript page/Section Linking way programe----------------
-function play(){
-   const home = document.getElementById('home');
-   home.classList.add('hidden');
-   const playground = document.getElementById('playground');
-   playground.classList.remove('hidden');
-}
+// function play(){
+//    const home = document.getElementById('home');
+//    home.classList.add('hidden');
+//    const playground = document.getElementById('playground');
+//    playground.classList.remove('hidden');
+// }
---
+++ script/utility.js
@@ -0,0 +1,13 @@
+function hideElement(elementId){
+    const element =document.getElementById(elementId);
+    element.classList.add('hidden');
+
+}
+function showElement(elementId){
+    const element=document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+function play(){
+    hideElement('home');
+    showElement('playground');
+}
