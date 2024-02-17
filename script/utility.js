8f23e36f8e01d99da5305dce0a1dd6d193dae094
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-17 12:23:31 +0600

    **Random alphabat generate done fn***

--- script/utility.js
+++ script/utility.js
@@ -13,9 +13,11 @@ function getRandomAlphabat(){
     //create random alphabat
     const alphabatString='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const alphbatArray = alphabatString.split('');
-    console.log(alphbatArray);
+  
     //create random alphabat
     const randomNumber =Math.random()*25;
     const index=Math.round(randomNumber);
-    console.log(index);
+
+    const alphabat = alphbatArray[index];
+    console.log(index,alphabat);
 }
