1658f07564260edf9ec7103e775128eda7aada8c
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-17 21:14:27 +0600

    *** Alph II If(Matched Condition) & 1 errore must sole bg-remove -- ***

--- script/utility.js
+++ script/utility.js
@@ -2,16 +2,21 @@ function hideElement(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 
-}
+};
 function showElement(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
-}
+};
 //2nd part  fn----------
 function setBackgroundColor(elementId) {
     const color = document.getElementById(elementId);
     color.classList.add('bg-orange-400');
-}
+};
+function removeBg(elementId) {
+    const color = document.getElementById(elementId);
+    color.classList.remove('bg-orange-400');
+};
+
 //1st part  fn------------------------------------------------------------
 function getRandomAlphabat() {
     //create random alphabat
@@ -24,23 +29,26 @@ function getRandomAlphabat() {
 
     const alphabat = alphbatArray[index];
     return alphabat;
-}
+};
 //Alphaa Clash II Project code start heree ------------------------------------------------
 
 
 document.addEventListener('keyup',function userInput(event){
     const userInput= event.key;
     const userInputFinal =userInput.toUpperCase(); //user input  store here
-    
+
     // Display Input store here
     const showAlphabat = document.getElementById('showAlphabat');
     const displayOutput=showAlphabat.innerText;
     console.log(displayOutput,userInputFinal); //user and display input show here
 
     //Check User Input and Display Input matched-----
-    if(userInputFinal==displayOutput){
+    if(userInputFinal===displayOutput){
         console.log("Matched keyword");
-    }else{
+        removeBg(displayOutput);
+        continueGame();
+    }
+    else{
         console.log("Not matched keyword");
     }
-})
+});
